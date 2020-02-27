6b85cdf9258e289befa6a299c8a9d849dd4b8dfb
Daniel Cheng (鄭郁邦) <j16sdiz@users.noreply.github.com>
2020-02-28 01:49:56 +0800

    tmux: enable utf8proc and fix typo

--- Formula/tmux.rb
+++ Formula/tmux.rb
@@ -3,12 +3,15 @@ class Tmux < Formula
   homepage "https://tmux.github.io/"
   url "https://github.com/tmux/tmux/releases/download/3.0a/tmux-3.0a.tar.gz"
   sha256 "4ad1df28b4afa969e59c08061b45082fdc49ff512f30fc8e43217d7b0e5f8db9"
+  revision 1
 
   bottle do
+    root_url "https://artifacts.apple.com/brew-bottles-core-binaries-local/homebrew-core/tmux/54a7f95c"
     cellar :any
-    sha256 "3d29caf7e2b87c9f1af575b4ec10af3e29c5de6979a8bd884153d9e8e1b69f20" => :catalina
-    sha256 "6cfd3987dcda2901a3ed423138db7782ea88a993e1409c4ebb9d647b1c007e8a" => :mojave
-    sha256 "c7c42853ca6a3a5ad6ab6544defee14e450643f5ace274b9cf00d348e8525993" => :high_sierra
+    rebuild 1
+    sha256 "206811cb9cafece9769d56ad470c004dc35d7085aedb0f02bcb889b209117d5d" => :nmos
+    sha256 "2c6e40600a6e8d417d923f89589bd658dd0c67ac42c05b0d4d83180f3b50e3fa" => :catalina
+    sha256 "6825064806995c50f570f60993145217147a2aa74e2fe43894ba4d5e52ee760a" => :mojave
   end
 
   head do
@@ -20,6 +23,7 @@ class Tmux < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on "utf8proc" => :build
   depends_on "libevent"
   depends_on "ncurses"
 
@@ -32,6 +36,7 @@ class Tmux < Formula
     system "sh", "autogen.sh" if build.head?
 
     args = %W[
+      --enable-utf8proc
       --disable-Dependency-tracking
       --prefix=#{prefix}
       --sysconfdir=#{etc}
